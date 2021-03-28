6876ca71c6b28bf786a3268eb883d993b0971087
Dhadhazi <dhadhazi@gmail.com>
2021-03-28 09:46:06 +0200

    Theme switcher animated

--- index.js
+++ index.js
@@ -5,19 +5,9 @@ const DARK_THEME = "dark";
 
 let theme = LIGHT_THEME;
 
-function changeElements(theme) {
-  if (theme === LIGHT_THEME) {
-    themeSwitchButton.children[0].classList.replace("fa-sun", "fa-moon");
-  }
-  if (theme === DARK_THEME) {
-    themeSwitchButton.children[0].classList.replace("fa-moon", "fa-sun");
-  }
-}
-
 function switchTheme() {
   theme = theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME;
   document.documentElement.setAttribute("data-theme", theme);
-  changeElements(theme);
 }
 
 themeSwitchButton.addEventListener("click", switchTheme);
