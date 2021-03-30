d68785c2abd26be1a1292ee2d525853002ab5290
Dhadhazi <dhadhazi@gmail.com>
2021-03-30 10:44:26 +0200

    List in object

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const themeSwitchButton = document.getElementById("themeSwitch-button");
 const backgroundImage = document.getElementById("backgroundImage");
+const addTodoButton = document.getElementById("addtodo");
+const todoListContainer = document.getElementById("todoListContainer");
 
 const LIGHT_THEME = "light";
 const DARK_THEME = "dark";
 
 let theme = LIGHT_THEME;
+
+let todoList = [
+  { name: "Complete online JavaScript Course", completed: true },
+  { name: "Jog around the park 3x", completed: false },
+];
 
 function changeElements(theme) {
   backgroundImage.src = `./images/bg-desktop-${theme}.jpg`;
@@ -17,3 +24,16 @@ function switchTheme() {
 }
 
 themeSwitchButton.addEventListener("click", switchTheme);
+
+// addTodoButton;.addEventListener("click",)
+
+todoList.forEach((todo) => {
+  const element = document.createElement("li");
+  element.classList.add("list__item");
+  if (todo.completed) element.classList.add("completed");
+  const circleEl = document.createElement("button");
+  circleEl.classList.add("circle");
+  element.appendChild(circleEl);
+  element.textContent = todo.name;
+  todoListContainer.prepend(element);
+});
