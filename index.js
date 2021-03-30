39558a92da1a4a94a8f296857917a18c1f03f432
Dhadhazi <dhadhazi@gmail.com>
2021-03-30 10:59:58 +0200

    Adds input element to list

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const themeSwitchButton = document.getElementById("themeSwitch-button");
 const backgroundImage = document.getElementById("backgroundImage");
-const addTodoButton = document.getElementById("addtodo");
+const addTodoButton = document.getElementById("addtodoButton");
 const todoListContainer = document.getElementById("todoListContainer");
+const inputElement = document.getElementById("additemInput");
 
 const LIGHT_THEME = "light";
 const DARK_THEME = "dark";
@@ -25,12 +26,27 @@ function switchTheme() {
 
 themeSwitchButton.addEventListener("click", switchTheme);
 
-// addTodoButton;.addEventListener("click",)
+function addTodo() {
+  todoList.push({ name: inputElement.value, completed: false });
+  refreshTodoList();
+  inputElement.value = "";
+}
 
-todoList.forEach((todo) => {
-  const element = document.createElement("li");
-  element.classList.add("list__item");
-  if (todo.completed) element.classList.add("completed");
-  element.innerHTML = '<button class="circle"></button>' + todo.name;
-  todoListContainer.prepend(element);
-});
+addTodoButton.addEventListener("click", addTodo);
+
+function refreshTodoList() {
+  todoListContainer.innerHTML = "";
+  displayTodoList();
+}
+
+function displayTodoList() {
+  todoList.forEach((todo) => {
+    const element = document.createElement("li");
+    element.classList.add("list__item");
+    if (todo.completed) element.classList.add("completed");
+    element.innerHTML = '<button class="circle"></button>' + todo.name;
+    todoListContainer.prepend(element);
+  });
+}
+
+displayTodoList();
