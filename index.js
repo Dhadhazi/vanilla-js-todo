21d72f53096866bef14034e1f7d3a098c95d1941
Dhadhazi <dhadhazi@gmail.com>
2021-03-31 11:03:51 +0200

    Todo can be completed

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const DARK_THEME = "dark";
 let theme = LIGHT_THEME;
 
 let todoList = [
-  { name: "Complete online JavaScript Course", completed: true },
-  { name: "Jog around the park 3x", completed: false },
+  { id: 1, name: "Complete online JavaScript Course", completed: true },
+  { id: 2, name: "Jog around the park 3x", completed: false },
 ];
 
 function changeElements(theme) {
@@ -27,16 +27,30 @@ function switchTheme() {
 themeSwitchButton.addEventListener("click", switchTheme);
 
 function addTodo() {
-  todoList.push({ name: inputElement.value, completed: false });
-  refreshTodoList();
+  todoList.push({
+    id: todoList.length,
+    name: inputElement.value,
+    completed: false,
+  });
+  updateDOM();
   inputElement.value = "";
 }
 
 addTodoButton.addEventListener("click", addTodo);
 
-function refreshTodoList() {
+function updateDOM() {
   todoListContainer.innerHTML = "";
   displayTodoList();
+}
+
+function toggleComplete(id) {
+  todoList.map((todo) => {
+    if (todo.id === id) {
+      todo.completed = !todo.completed;
+    }
+    return todo;
+  });
+  updateDOM();
 }
 
 function displayTodoList() {
@@ -44,7 +58,7 @@ function displayTodoList() {
     const element = document.createElement("li");
     element.classList.add("list__item");
     if (todo.completed) element.classList.add("completed");
-    element.innerHTML = `<button class="circle"></button>${todo.name}<button class="close"><img src="images/icon-cross.svg"></button>`;
+    element.innerHTML = `<button class="circle" onclick="toggleComplete(${todo.id})"></button>${todo.name}<button class="close"><img src="images/icon-cross.svg"></button>`;
     todoListContainer.prepend(element);
   });
 }
